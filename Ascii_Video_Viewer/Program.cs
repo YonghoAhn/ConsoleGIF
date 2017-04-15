93cadb71a4134b0edfc638197c26f68eddf532d6
YonghoAhn <hrh1110@naver.com>
2017-04-15 15:16:21 +0900

    Complete

--- Ascii_Video_Viewer/Program.cs
+++ Ascii_Video_Viewer/Program.cs
@@ -3,13 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
 
 namespace Ascii_Video_Viewer
 {
     class Program
     {
+        private static string[] _AsciiChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", " " };
+        private static string _Content;
+        private static string Path;
         static void Main(string[] args)
         {
+
+            Console.CursorSize = 8;
+            Path = Console.ReadLine();
+            GifImage a = new GifImage(Path)
+            {
+                ReverseAtEnd = false
+            };
+            while (true)
+            {
+                GenerateAscii(a.GetNextFrame());
+            }
+            Console.WriteLine("asdf");
+            Console.ReadKey();
+        }
+
+        private static string ConvertToAscii(Bitmap image)
+        {
+            Boolean toggle = false;
+            StringBuilder sb = new StringBuilder();
+
+            for (int h = 0; h < image.Height; h++)
+            {
+                for (int w = 0; w < image.Width; w++)
+                {
+                    Color pixelColor = image.GetPixel(w, h);
+                    //Average out the RGB components to find the Gray Color
+                    int red = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                    int green = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                    int blue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                    Color grayColor = Color.FromArgb(red, green, blue);
+
+                    //Use the toggle flag to minimize height-wise stretch
+                    if (!toggle)
+                    {
+                        int index = (grayColor.R * 10) / 255;
+                        sb.Append(_AsciiChars[index]);
+                    }
+                }
+                if (!toggle)
+                {
+                    sb.Append("\r\n");
+                    toggle = true;
+                }
+                else
+                {
+                    toggle = false;
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private static void GenerateAscii(Image i)
+        {
+            //Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Bitmap image = new Bitmap(i);
+            image = GetReSizedImage(image, 80);
+            _Content = ConvertToAscii(image);
+            Console.Write(_Content);
+        }
+
+        private static Bitmap GetReSizedImage(Bitmap inputBitmap, int asciiWidth)
+        {
+            int asciiHeight = 0;
+            //폭을 줄였으니 높이도 계산한다.
+            asciiHeight = (int)Math.Ceiling((double)inputBitmap.Height * asciiWidth / inputBitmap.Width);
+
+            //폭*높이에 맞는 새 Bitmap 생성
+            Bitmap result = new Bitmap(asciiWidth, asciiHeight);
+            Graphics g = Graphics.FromImage((Image)result);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.DrawImage(inputBitmap, 0, 0, asciiWidth, asciiHeight);
+            g.Dispose();
+            return result;
         }
     }
 }
